3ed02682b7ee7c885594145ebb0d29c143b73a46
Tim Beals <tim.beals@gmail.com>
2017-02-26 17:25:54 -0800

    basic setup and pod install

---
+++ Lottie_swipe_animation/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Lottie_swipe_animation
+//
+//  Created by Tim Beals on 2017-02-26.
+//  Copyright © 2017 Tim Beals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Animation"
+        view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

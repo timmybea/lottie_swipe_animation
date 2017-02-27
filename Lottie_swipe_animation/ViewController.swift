0b0eea1c4061cf118c53c9fb18be71ec6a9d8ebf
Tim Beals <tim.beals@gmail.com>
2017-02-27 08:29:53 -0800

    create scroll view with label and use to calculate animation progress

--- Lottie_swipe_animation/ViewController.swift
+++ Lottie_swipe_animation/ViewController.swift
@@ -7,25 +7,78 @@
 //
 
 import UIKit
+import Lottie
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
     
-
+    let stringArray = ["<- Swipe to begin", "Get out of bed", "Don't forget to clean.", "Use water...", "...then eat TOAST", "...and go to work"]
     
+    let animationView: LOTAnimationView = {
+        let view = LOTAnimationView.animationNamed("tutorial")
+        view?.backgroundColor = UIColor.white
+        view?.loopAnimation = true
+        view?.contentMode = .scaleAspectFill
+        return view!
+    }()
+    
+    let scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.backgroundColor = UIColor.clear
+        view.isPagingEnabled = true
+        
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Animation"
         view.backgroundColor = UIColor.white
+        
+        self.view.addSubview(animationView)
+        self.view.addSubview(scrollView)
+        
+        animationView.frame = CGRect(x: 0, y: 200, width: self.view.frame.width, height: 300)
+        scrollView.frame = self.view.frame
+
+//        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.handlePan(recognizer:)))
+//        self.view.addGestureRecognizer(panGesture)
+        
+        setupScrollView()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupScrollView() {
+        scrollView.delegate = self
+        scrollView.contentSize = CGSize(width: view.frame.width * 6, height: view.frame.height)
+        
+        for (i, string) in stringArray.enumerated() {
+            
+            let label = UILabel()
+            label.frame = CGRect(x: scrollView.center.x + (CGFloat(i) * scrollView.frame.width) - CGFloat(130), y: scrollView.frame.height * CGFloat(0.75), width: 260, height: 50)
+            label.font = UIFont.boldSystemFont(ofSize: 24)
+            label.text = string
+            label.textAlignment = .center
+            scrollView.addSubview(label)
+            
+        }
     }
+    
+//    func handlePan(recognizer: UIPanGestureRecognizer) {
+//        
+//        let translation = recognizer.translation(in: self.view)
+//        let progress = translation.x / self.view.bounds.width
+//        
+//        animationView.animationProgress = progress
+//    }
 
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
 
+        let progress = scrollView.contentOffset.x / scrollView.contentSize.width
+        animationView.animationProgress = progress
+    }
 }
 
